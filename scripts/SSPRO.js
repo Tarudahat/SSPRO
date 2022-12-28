0d350239a5f0e7c62b19c7358a03a595cdec81c9
Tarudahat <taruzahat@gmail.com>
2022-12-28 22:42:17 +0100

    add exit btn setting page

--- scripts/SSPRO.js
+++ scripts/SSPRO.js
@@ -2,22 +2,26 @@
 
 console.log("SSPRO.js ran");
 
-
 function go_2_settings(){
-    console.log(window.location.href.split(".be")[0]+".be");
-    window.location.href = browser.runtime.getURL("html_pages/SSPRO_settings.html");
+    var temp_str = "{\"sspro_home_url\": \""+window.location.href.split(".be")[0]+".be"+"\"}";
+    var temp_obj = JSON.parse(temp_str);
+    browser.storage.local.set(temp_obj);
+    setTimeout(function(){window.location.href = browser.runtime.getURL("html_pages/SSPRO_settings.html");},100);
 }
 
 //create some css stuff
 var css_element = document.createElement("style");
-css_element.innerHTML = ".smsc-topnav .topnav__btn--icon--SSPRO{background-image: url('"+browser.runtime.getURL("icons/SSPRO_setting.png")+"');}";
+css_element.innerHTML = ".smsc-topnav .navbar_btn_icon_SSPRO{background-image: url('"+browser.runtime.getURL("icons/SSPRO_setting.png")+"');}";
+css_element.innerHTML += ".topbar{display: flex; align-items: center; padding: 0;height: 48px;min-height: 48px;background: #f5ff15;}";
 document.head.appendChild(css_element);
 
 
 //add the sspro btn to the topnav
 var topnav = document.getElementsByTagName("nav")[0];
+topnav.className="topbar";
 var sspro_settings_btn = document.createElement("button");
-sspro_settings_btn.className="topnav__btn topnav__btn--icon topnav__btn--icon--SSPRO";
+sspro_settings_btn.className="topnav__btn topnav__btn--icon .navbar_btn_icon_SSPRO";
 sspro_settings_btn.title="SSPRO instellingen";
 sspro_settings_btn.onclick=go_2_settings;
 topnav.appendChild(sspro_settings_btn);
+
--- scripts/SSPRO_SETTINGS.js
+++ scripts/SSPRO_SETTINGS.js
@@ -1,8 +1,12 @@
 "use strict"
 
-var home_url;
+async function get_home_url(){
+    var a;
+    var home_url;
+    await browser.storage.local.get("sspro_home_url").then((a)=>{home_url=a.sspro_home_url;});
+    document.getElementById("exit_btn").href=home_url+"/logout";
+}
 
-document.getElementById("exit_btn").href=home_url+"/logout";
-document.getElementsByTagName("style")[0].innerHTML.replace("=1=",home_url+"/smsc/svg/exit_ic_white/exit_ic_white_24x24.svg"); 
-
+get_home_url();
+document.getElementsByTagName("style")[0].innerHTML=document.getElementsByTagName("style")[0].innerHTML.replace("=1=","https://static6.smart-school.net/smsc/svg/exit_ic_white/exit_ic_white_24x24.svg");
 document.getElementById("rtrn_btn").addEventListener("click",()=>{history.back()});
