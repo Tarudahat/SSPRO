fa43a35ca53606f588205d7e0b5eef3708b187fe
Tarudahat <taruzahat@gmail.com>
2022-12-29 20:47:23 +0100

    add option bool toggle buttons

--- scripts/SSPRO.js
+++ scripts/SSPRO.js
@@ -9,19 +9,41 @@ function go_2_settings(){
     setTimeout(function(){window.location.href = browser.runtime.getURL("html_pages/SSPRO_settings.html");},100);
 }
 
+async function rmv_dvj(){
+    var show_bool;
+    await browser.storage.local.get("show_dvj").then((show_bool)=>{if(show_bool.show_dvj){}else{document.getElementById("homepage__block--student-support").remove();}; console.log(show_bool);});
+}
+
+function scrape_score_nodes(){
+    console.log(document.getElementsByClassName("evaluation-graphic__progress-ring__value")[0]);
+    var list_scores = document.getElementsByClassName("progress-ring__content");
+    console.log(list_scores);
+    var purple_scores = [];
+    for(var score in list_scores){
+    
+        if (Number(score.innerText.split("&")[0])>=95)
+        {
+            purple_scores.append(score);
+            console.log(score);
+        }
+    }
+}
+
 //create some css stuff
 var css_element = document.createElement("style");
 css_element.innerHTML = ".smsc-topnav .navbar_btn_icon_SSPRO{background-image: url('"+browser.runtime.getURL("icons/SSPRO_setting.png")+"');}";
-css_element.innerHTML += ".topbar{display: flex; align-items: center; padding: 0;height: 48px;min-height: 48px;background: #f5ff15;}";
 document.head.appendChild(css_element);
-
 
 //add the sspro btn to the topnav
 var topnav = document.getElementsByTagName("nav")[0];
-topnav.className="topbar";
 var sspro_settings_btn = document.createElement("button");
-sspro_settings_btn.className="topnav__btn topnav__btn--icon .navbar_btn_icon_SSPRO";
+sspro_settings_btn.className="topnav__btn topnav__btn--icon navbar_btn_icon_SSPRO";
 sspro_settings_btn.title="SSPRO instellingen";
 sspro_settings_btn.onclick=go_2_settings;
 topnav.appendChild(sspro_settings_btn);
+
+//apply options
+/*<div class="evaluation-graphic evaluation-graphic--large evaluation-graphic--percentage side-panel__content__score__large-ring c-blue-combo--100"><div style="width: 128px; height: 128px;" class="progress-ring"><svg width="128px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 128" class="progress-ring__svg"><circle cx="64" cy="64" r="59" class="progress-ring__svg__stroke" style="stroke: var(--c-blue--200); stroke-width: 10px;"></circle><circle cx="64" cy="64" r="59" class="progress-ring__svg__bar" style="stroke: var(--c-blue--500); stroke-width: 10px; stroke-linecap: round; stroke-dasharray: 370.708px, 370.708px; stroke-dashoffset: 26.0354px;"></circle></svg><div class="progress-ring__content"><div class=""><span class="evaluation-graphic__progress-ring__description">9,5/10</span><span class="evaluation-graphic__progress-ring__value">95%</span></div></div></div></div>*/
+
+if(document.contains(document.getElementById("homepage__block--student-support"))){rmv_dvj();};
 
--- scripts/SSPRO_SETTINGS.js
+++ scripts/SSPRO_SETTINGS.js
@@ -7,6 +7,36 @@ async function get_home_url(){
     document.getElementById("exit_btn").href=home_url+"/logout";
 }
 
+async function sync_btn2bool(bool_name)
+{
+    var btn_classlist = document.getElementById(bool_name+"_btn").classList;
+    var show_bool;
+    await browser.storage.local.get(bool_name).then((show_bool)=>{
+        if(show_bool[bool_name]){
+            if (btn_classlist.contains("off")){btn_classlist.remove("off");}
+        }else{
+            btn_classlist.add("off");
+        };
+    });
+}
+
+async function toggle_bool(bool_name)
+{
+    var btn_classlist = document.getElementById(bool_name+"_btn").classList;
+    btn_classlist.toggle("off")
+    if (btn_classlist.contains("off"))
+    {
+        browser.storage.local.set(JSON.parse("{\""+bool_name+"\":false}"));
+    }else
+    {
+        browser.storage.local.set(JSON.parse("{\""+bool_name+"\":true}"));
+    }
+}
+
 get_home_url();
 document.getElementsByTagName("style")[0].innerHTML=document.getElementsByTagName("style")[0].innerHTML.replace("=1=","https://static6.smart-school.net/smsc/svg/exit_ic_white/exit_ic_white_24x24.svg");
 document.getElementById("rtrn_btn").addEventListener("click",()=>{history.back()});
+
+sync_btn2bool("show_dvj");
+document.getElementById("show_dvj_btn").addEventListener("click",()=>{toggle_bool("show_dvj");sync_btn2bool("show_dvj")});
+
